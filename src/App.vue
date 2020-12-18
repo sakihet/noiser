6c22e5e66de50fcfe5182a78fd8d26a55ea87e15
saki <sakihet@gmail.com>
2020-12-19 00:45:46 +0900

    add AppCanvas component

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,15 @@
       :headerText="name"
       :headerDescription="description"
     />
+    <AppCanvas
+      :height="parseInt(height)"
+      :width="parseInt(width)"
+    />
   </div>
 </template>
 
 <script>
+import AppCanvas from './components/AppCanvas'
 import AppHeader from './components/AppHeader'
 import {name} from '../package.json'
 import {description} from '../package.json'
@@ -15,12 +20,15 @@ import {description} from '../package.json'
 export default {
   name: 'App',
   components: {
+    AppCanvas,
     AppHeader
   },
   data () {
     return {
       name: name[0].toUpperCase() + name.slice(1),
-      description: description
+      description: description,
+      height: 256,
+      width: 256
     }
   }
 }
---
+++ src/components/AppCanvas.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <canvas
+      :height="height"
+      :width="width"
+    >
+    </canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppCanvas',
+  props: {
+    height: Number,
+    width: Number
+  }
+}
+</script>
+
+<style scoped>
+canvas {
+  border: 1px dotted;
+}
+</style>
