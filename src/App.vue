c08c94e6f31c473a229f04fe44db401fd16422b3
saki <sakihet@gmail.com>
2020-12-19 11:29:40 +0900

    add AppInput component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
       :headerText="name"
       :headerDescription="description"
     />
+    <AppInput
+      :value="height"
+      @input="height = $event"
+    />
+    <AppInput
+      :value="width"
+      @input="width = $event"
+    />
     <AppCanvas
       :height="parseInt(height)"
       :width="parseInt(width)"
@@ -16,6 +24,7 @@
 <script>
 import AppCanvas from './components/AppCanvas'
 import AppHeader from './components/AppHeader'
+import AppInput from './components/AppInput'
 import {name} from '../package.json'
 import {description} from '../package.json'
 import {version} from '../package.json'
@@ -24,16 +33,19 @@ export default {
   name: 'App',
   components: {
     AppCanvas,
-    AppHeader
+    AppHeader,
+    AppInput
   },
   data () {
     return {
       name: name[0].toUpperCase() + name.slice(1),
       description: description,
       version: version,
-      height: 256,
-      width: 256
+      height: "256",
+      width: "256"
     }
+  },
+  methods: {
   }
 }
 </script>
---
+++ src/components/AppInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input
+      type="number"
+      :value="value"
+      @input="$emit('input', $event.target.value)"
+    >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppInput',
+  props: {
+    value: {
+      type: String
+    }
+  },
+  emits: ['input']
+}
+</script>
+
+<style scoped>
+</style>
