5e5eb171fe549cb07eb5221117177b871fdfe72e
saki <sakihet@gmail.com>
2020-12-19 16:56:03 +0900

    update draw

--- src/components/AppCanvas.vue
+++ src/components/AppCanvas.vue
@@ -26,25 +26,27 @@ export default {
       return r
     },
     draw () {
-      const ctx = this.$el.childNodes[0].getContext('2d')
-      ctx.fillStyle = this.color
-      const w = this.width
-      const h = this.height
-      const th = this.threshold
-      ctx.clearRect(0, 0, w, h)
-      ctx.fillRect(0, 0, w, h)
-      const inputImageData = ctx.getImageData(0, 0, w, h)
-      const inputData = inputImageData.data
-      const outputImageData = ctx.getImageData(0, 0, w, h)
-      const outputData = outputImageData.data
-      for( let i = 0; i < w * h * 4; i = i + 4){
-        let r = this.myrand(-th, th)
-        outputData[i] = inputData[i] + r
-        outputData[i+1] = inputData[i+1] + r
-        outputData[i+2] = inputData[i+2] + r
-        outputData[i+3] = 255
+      if (1 <= this.width && 1 <= this.height) {
+        const ctx = this.$el.childNodes[0].getContext('2d')
+        ctx.fillStyle = this.color
+        const w = this.width
+        const h = this.height
+        const th = this.threshold
+        ctx.clearRect(0, 0, w, h)
+        ctx.fillRect(0, 0, w, h)
+        const inputImageData = ctx.getImageData(0, 0, w, h)
+        const inputData = inputImageData.data
+        const outputImageData = ctx.getImageData(0, 0, w, h)
+        const outputData = outputImageData.data
+        for( let i = 0; i < w * h * 4; i = i + 4){
+          let r = this.myrand(-th, th)
+          outputData[i] = inputData[i] + r
+          outputData[i+1] = inputData[i+1] + r
+          outputData[i+2] = inputData[i+2] + r
+          outputData[i+3] = 255
+        }
+        ctx.putImageData(outputImageData, 0, 0)
       }
-      ctx.putImageData(outputImageData, 0, 0)
     },
     emitDataURL () {
       const dataURL = this.$el.childNodes[0].toDataURL('image/png')
