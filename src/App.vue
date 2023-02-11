d11d1e817c52baa5ea4c24a3515b8089f2813b62
saki <sakihet@gmail.com>
2023-02-11 17:36:54 +0900

    npm create vite@latest .

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,30 @@
-<template>
-  <div id="app">
-    <div class="container">
-      <AppHeader
-        :headerText="name"
-        :headerDescription="description"
-      />
-      <InputLabeled
-        :value="color"
-        labelName="color"
-        inputId="color"
-        inputType="color"
-        @input="color = $event"
-      />
-      <InputLabeled
-        :value="threshold"
-        labelName="threshold"
-        inputId="threshold"
-        inputType="range"
-        @input="threshold = $event"
-      />
-      <InputLabeled
-        :value="height"
-        labelName="height"
-        inputId="height"
-        inputType="number"
-        @input="height = $event"
-      />
-      <InputLabeled
-        :value="width"
-        labelName="width"
-        inputId="width"
-        inputType="number"
-        @input="width = $event"
-      />
-      <AppAnchor
-        :href="dataURL"
-        download="image.png"
-        text="Download"
-      />
-      <AppCanvas
-        :color="color"
-        :threshold="parseInt(threshold)"
-        :height="parseInt(height)"
-        :width="parseInt(width)"
-        @updated="updateDataURL($event)"
-      />
-      <AppAnchor
-        href="https://github.com/sakihet/noiser"
-        target="_blank"
-        text="github"
-      />
-      <p>version: {{ version }}</p>
-    </div>
-  </div>
-</template>
-
-<script>
-import AppAnchor from './components/AppAnchor'
-import AppCanvas from './components/AppCanvas'
-import AppHeader from './components/AppHeader'
-import InputLabeled from './components/InputLabeled'
-import {name} from '../package.json'
-import {description} from '../package.json'
-import {version} from '../package.json'
-
-export default {
-  name: 'App',
-  components: {
-    AppAnchor,
-    AppCanvas,
-    AppHeader,
-    InputLabeled
-  },
-  data () {
-    return {
-      name: name[0].toUpperCase() + name.slice(1),
-      description: description,
-      version: version,
-      color: "#1abc9c",
-      threshold: "32",
-      height: "256",
-      width: "256",
-      dataURL: ""
-    }
-  },
-  methods: {
-    updateDataURL (dataURL) {
-      this.dataURL = dataURL
-    }
-  }
-}
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style>
-#app {
-  text-align: center;
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-.container {
-  display: flex;
-  flex-direction: column;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
